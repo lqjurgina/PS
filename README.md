9fa4d064b292d24b1d57edb732d0da5dac116fc7
Darlei Schmegel <darleischmegel94@gmail.com>
2020-11-05 16:08:06 -0300

    alterando Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PS
 Trabalho de Ps
+# Critério de uso e nomeação 
+ função:    primeira letra minuscula, 
+            caso tenha duas palavras, segunda usar maiuscula para diferenciar
+            EX: funçãoFazTalCoisa
+ Métodos:   retorno e alteração de variáveis
+            EX: getVariavel, setVariavel
+ Classes:   Primeira letra maiuscula 
+            caso tenha duas palavras, segunda também é maiuscula 
+            EX: Classe, ClasseFazTalCoisa
--- src/trabalhops/Registradores.java
+++ src/trabalhops/Registradores.java
@@ -11,4 +11,88 @@ package trabalhops;
  */
 public class Registradores {
     
+    
+    private short PC, SP, ACC, RI, RE;
+    private byte MOP;
+    
+    //Inicializacao da classe registradores
+    Registradores() {
+        PC = 0;
+        SP = 0;
+        ACC = 0;
+        RI = 0;
+        RE = 0;
+        MOP = 0;
+    }
+    // Retorna todos os registradores menos o MOP
+    public short[] getRegistradores() {
+        short[] regs = new short[5];
+        regs[0] = this.PC;
+        regs[1] = this.SP;
+        regs[2] = this.ACC;
+        regs[3] = this.RI;
+        regs[4] = this.RE;
+
+        return regs;
+    }
+    
+    // GET e SET do SP
+    public void setSP(short a) {
+        this.SP = a;
+        //Memory.memoria.addBloco(Integer.toString(this.getSP()), 996);
+    }
+    public short getSP() {
+        return this.SP;
+    }
+    
+    // GET e SET do ACC
+    public void setACC(short a) {
+        this.ACC = a;
+        //Memory.memoria.addBloco(Integer.toString(this.getACC()), 997);
+    }
+    public short getACC() {
+        return this.ACC;
+    }
+    
+    // GET e SET do registrador RI
+    public void setRI(short a) {
+        this.RI = a;
+        //Memory.memoria.addBloco(Integer.toString(this.getRI()), 998);
+    }
+    public short getRI() {
+        return this.RI;
+    }
+    
+    // GET E SET do registrador RE
+    public void setRE(short a) {
+        this.RE = a;
+        //Memory.memoria.addBloco(Integer.toString(this.getRE()), 999);
+    }
+    public short getRE() {
+        return this.RE;
+    }
+
+    //	GET e SET e INC do registrador PC
+    public void setPC(short a) {
+        this.PC = a;
+        //Memory.memoria.addBloco(Integer.toString(this.getPC()), 995);
+    }
+    public short getPC() {
+        return this.PC;
+    }
+    
+    // Metodo que testa qual instrucao e incremneta o PC
+    public void incPC(int operacao) {// Essa parte tem que mudar para binario
+        if (operacao == (2) || operacao == (0) || operacao == (5)
+                || operacao == (1) || operacao == (4) || operacao == (15)
+                || operacao == (10) || operacao == (3) || operacao == (14)
+                || operacao == (12) || operacao == (7) || operacao == (6)
+                || operacao == (8)) {
+            this.PC += 2;
+        } else if (operacao == (16) || operacao == (11)) {
+            this.PC++;
+        } else if (operacao == (13)) {
+            this.PC += 3;
+        }
+    }
 }
